b45ebe640ceae855cd37641bc5bfe4e6315adf46
mujib2953 <ansarimujiburrehman@gmail.com>
2019-12-25 18:02:03 +0530

    Unused file removed.

---
+++ test.js
@@ -0,0 +1,17 @@
+// import { SpellOut } from "./SpellOut.js";
+const { SpellOut } = require("./SpellOut.js");
+const sampledata = [
+    // "0",
+    // "12",
+    // "32645732",
+    // "15000",
+    // "111111",
+    // "76435730",
+    // "121212123",
+    "12345678"
+    // "a"
+];
+
+sampledata.forEach((s) => {
+    console.log(`${s} => "${SpellOut(s, "U")}"`);
+});
